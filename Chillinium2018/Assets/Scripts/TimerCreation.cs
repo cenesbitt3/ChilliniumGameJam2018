278100fc9528c48eb4642471c14d1956b9da57f3
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2018-10-12 19:01:23 -0500

    Made Timer script

---
+++ Chillinium2018/Assets/Scripts/TimerCreation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerCreation : MonoBehaviour {
+
+    public float timer = 3.0f;
+    public GameObject creationPosition;
+    public GameObject grid;
+
+    public void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            Instantiate(grid, creationPosition.transform.position, Quaternion.identity);
+            timer = 3.0f;
+        }
+    }
+}
