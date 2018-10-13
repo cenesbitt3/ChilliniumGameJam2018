0bc99c428179c31aa0f02be0bd8f8557a8015a22
Unknown <cenesbitt3@gmail.com>
2018-10-12 21:44:53 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Chillinium2018/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Camera curCamera;
+    float camRayLength = 100f;
+    int CubeMask;
+    void Awake()
+    {
+        CubeMask = LayerMask.GetMask("Float");
+    }
+
+
+    void Update()
+    {
+        Ray camRay = curCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit TargetHit;
+
+        if (Physics.Raycast(camRay, out TargetHit, camRayLength, CubeMask))
+        {
+        }
+    }
+}
