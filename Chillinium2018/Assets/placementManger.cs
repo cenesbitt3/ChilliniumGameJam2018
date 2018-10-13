227fb8dbf8d9d85897a48622176bee09134206a1
Unknown <cenesbitt3@gmail.com>
2018-10-13 01:46:51 -0500

    finished mangager

---
+++ Chillinium2018/Assets/placementManger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placementManger : MonoBehaviour {
+
+    public GameObject[] placmentObjects;
+    public int[] colors;
+    public bool[] filled;
+    public int[] placment;
+    private void Update()
+    {
+        for(int i =0; i < placmentObjects.Length;i++)
+        {
+             colors[i] = placmentObjects[i].gameObject.GetComponent<placementObj>().color;
+             filled[i] = placmentObjects[i].gameObject.GetComponent<placementObj>().filled;
+              placment[i] = placmentObjects[i].gameObject.GetComponent<placementObj>().placement;
+        }
+    }
+}
