071e1d80637127d3074782ad91d78e13f1f238eb
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2018-10-13 00:13:01 -0500

    Added Abi art, implemented grid moving

--- Chillinium2018/Assets/Scripts/TimerCreation.cs
+++ Chillinium2018/Assets/Scripts/TimerCreation.cs
@@ -7,9 +7,13 @@ public class TimerCreation : MonoBehaviour {
     public float timer = 5.0f;
     public GameObject creationPosition;
     public GameObject grid;
+    public Transform ring;
 
     public void Update()
     {
-        if(grid)
+        if (!GameObject.FindGameObjectWithTag("Grid")) {
+            GameObject newGrid = Instantiate(grid, creationPosition.transform.position, creationPosition.transform.rotation);
+            newGrid.transform.parent = ring;
+        }
     }
 }
