c0c4622e8f1594a00462c6a9feb65c583de5f0ad
Unknown <cenesbitt3@gmail.com>
2018-10-13 03:14:56 -0500

    everything

--- Chillinium2018/Assets/Scripts/EnemyGrid.cs
+++ Chillinium2018/Assets/Scripts/EnemyGrid.cs
@@ -5,36 +5,42 @@ using UnityEngine;
 public class EnemyGrid : MonoBehaviour {
 
     public GameObject[] places = new GameObject[9];
+    bool isFilled;
 
-    int colorChoice;
-    int filledChoice;
-    bool filled;
+    placementManger pmanger;
+
+    int[] colorChoice = new int[9];
+    int[] filledChoice = new int[9];
+    bool[] filled = new bool[9];
     GameObject shape;
-
+    Spawner spawner;
     Manager managerScript;
 
     public void Awake()
     {
+        spawner = FindObjectOfType<Spawner>();
+        pmanger = FindObjectOfType<placementManger>();
         managerScript = FindObjectOfType<Manager>();
-
+        spawner.Spawn();
         for (int i = 0; i < places.Length; i++) {
-            colorChoice = Random.Range(0, 3);
-            filledChoice = Random.Range(0, 2);
+            colorChoice[i] = Random.Range(0, 3);
 
-            if (filledChoice == 1)
+            filledChoice[i] = Random.Range(0, 2);
+
+            if (filledChoice[i] == 1)
             {
-                filled = false;
-                places[i].SetActive(false);
-                continue;
+                isFilled = true; // switvh to false later
+               // places[i].SetActive(false);
+                //continue;
             }
-            else if(filledChoice == 1){
-                filled = true;
+            else if(filledChoice[i] == 1){
+                isFilled = true;
                 places[i].SetActive(true);
             }
-
+            filled[i] = isFilled;
             MeshRenderer mesh = places[i].GetComponent<MeshRenderer>();
 
-            switch (colorChoice) {
+            switch (colorChoice[i]) {
                 case 0: // Red material
                     mesh.material = managerScript.mats[0];
                     break;
@@ -46,7 +52,7 @@ public class EnemyGrid : MonoBehaviour {
                     break;
             }
             EnemyPlacementManager enemyPlace = places[i].GetComponent<EnemyPlacementManager>();
-            enemyPlace.FillValues(colorChoice, i, filled);
+            enemyPlace.FillValues(colorChoice[i], i, isFilled);
         }
     }
 
@@ -55,7 +61,12 @@ public class EnemyGrid : MonoBehaviour {
         if (other.gameObject.CompareTag("ScreenCollider")) {
             gameObject.transform.parent = null;
             // Run check the the classes
-
+            for (int i = 0; i < pmanger.colors.Length; i++) {
+                if (pmanger.colors[i] == colorChoice[i])
+                {
+                    Debug.Log("working");
+                }
+            }
         }
     }
 
